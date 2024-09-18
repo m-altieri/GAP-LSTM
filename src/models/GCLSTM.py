8fbe6fb678c7c916fc1ffce0d2c475d39caab0c0
m-altieri <massimiliano.altieri@uniba.it>
2024-09-18 07:03:34 -0400

    polished gclstm

--- src/models/GCLSTM.py
+++ src/models/GCLSTM.py
@@ -1,4 +1,4 @@
-"""GCLSTM.py
+"""
 GCLSTM.
 Questo modello può essere lanciato anche nelle sue versioni ablate:
 GCLSTM-noconv: Conv2D disattivata completamente
@@ -10,13 +10,9 @@ GCLSTM-noskip: salta la skip connection
 
 import sys
 import scipy
-import logging
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import *
 
-# sys.path.append("/lustrehome/altieri/research/src")
-# sys.path.append("/lustrehome/altieri/research/src/lib")
 sys.path.append("../lib")
 from lib.spektral_utilities import *
 from lib.spektral_gcn import GraphConv
@@ -24,13 +20,11 @@ from lib.spektral_gcn import GraphConv
 
 class GCLSTM(tf.keras.Model):
     def __init__(self, H, P, adj, N, node=0, ablation=None, **kwargs):
-        super(GCLSTM, self).__init__()
+        super().__init__()
 
         self.w = None
         if kwargs.get("summary_path", False):
             self.w = tf.summary.create_file_writer(kwargs.get("summary_path"))
-        self.logger = logging.getLogger(__name__)
-        self.logger.info(__name__ + " initializing with node " + str(node))
 
         self.H = H
         self.adj = adj
@@ -40,24 +34,24 @@ class GCLSTM(tf.keras.Model):
         self.gcn1 = GraphConv(32, activation="relu", name="gcn1")
         self.gcn2 = GraphConv(16, activation="relu", name="gcn2")
 
-        self.conv1 = Conv1D(
+        self.conv1 = tf.keras.layers.Conv1D(
             4, (3), padding="same", data_format="channels_first", name="conv1"
         )  # 1d
-        self.conv2 = Conv1D(
+        self.conv2 = tf.keras.layers.Conv1D(
             4, (3), padding="same", data_format="channels_first", name="conv2"
         )  # 1d
 
-        # self.bn = tf.keras.layers.BatchNormalization(axis=1)
+        self.bn = tf.keras.layers.BatchNormalization()
 
-        self.pooling = AveragePooling1D(
+        self.pooling = tf.keras.layers.AveragePooling1D(
             (2), padding="same", data_format="channels_first", name="pooling"
         )  # 1d
 
-        self.lstm1 = LSTM(128, return_sequences=True, name="lstm1")
-        self.lstm2 = LSTM(128, name="lstm2")
+        self.lstm1 = tf.keras.layers.LSTM(128, return_sequences=True, name="lstm1")
+        self.lstm2 = tf.keras.layers.LSTM(128, name="lstm2")
 
-        self.dropout = Dropout(0.5, name="dropout")
-        self.out = Dense(P, name="out")
+        self.dropout = tf.keras.layers.Dropout(0.5, name="dropout")
+        self.out = tf.keras.layers.Dense(P, name="out")
 
         self.ablation = ablation
         self.gcnmaps = tf.Variable(
@@ -89,18 +83,17 @@ class GCLSTM(tf.keras.Model):
     def set_node(self, n):
         assert n < len(self.adj)
         self.node = n
-        self.logger.info(f"Node {n} set for training in {__name__}.")
+        print(f"Node {n} set for training in {__name__}.")
 
-    def call(self, inputs, training=False):
-        self.logger.debug("Inputs: " + str(inputs.shape))  # [B,H,N,F]
+    @staticmethod
+    def compute_node_features(inputs):
         B, H, N, F = inputs.shape
-
-        node_features = tf.stack(
+        return tf.stack(
             [
                 tf.math.reduce_mean(inputs[..., 0], axis=(1)),
-                tf.math.reduce_mean(inputs[:, self.H // 2 :, :, 0], axis=(1)),
+                tf.math.reduce_mean(inputs[:, H // 2 :, :, 0], axis=(1)),
                 tf.math.reduce_std(inputs[..., 0], axis=(1)),
-                tf.math.reduce_std(inputs[:, self.H // 2 :, :, 0], axis=(1)),
+                tf.math.reduce_std(inputs[:, H // 2 :, :, 0], axis=(1)),
                 tf.numpy_function(
                     lambda x: np.nan_to_num(
                         np.array(
@@ -109,7 +102,7 @@ class GCLSTM(tf.keras.Model):
                                     scipy.stats.skew(
                                         x[b, :, n, 0], axis=None, nan_policy="raise"
                                     )
-                                    for n in range(self.N)
+                                    for n in range(N)
                                 ]
                                 for b in range(B)
                             ],
@@ -127,7 +120,7 @@ class GCLSTM(tf.keras.Model):
                                     scipy.stats.kurtosis(
                                         x[b, :, n, 0], axis=None, nan_policy="raise"
                                     )
-                                    for n in range(self.N)
+                                    for n in range(N)
                                 ]
                                 for b in range(B)
                             ],
@@ -142,8 +135,8 @@ class GCLSTM(tf.keras.Model):
                         np.array(
                             [
                                 [
-                                    np.polyfit(np.arange(self.H), x[b, :, n, 0], 1)[0]
-                                    for n in range(self.N)
+                                    np.polyfit(np.arange(H), x[b, :, n, 0], 1)[0]
+                                    for n in range(N)
                                 ]
                                 for b in range(B)
                             ],
@@ -156,20 +149,24 @@ class GCLSTM(tf.keras.Model):
             ],
             axis=2,
         )
+
+    def call(self, inputs, training=False):
+
+        node_features = self.compute_node_features(inputs)  # [B,N,7]
+
         if self.ablation == "nosummary":
             node_features = node_features[..., :1]
 
         if not training:  # save summaries for interpretation
             self.summaries.assign(tf.concat([self.summaries, node_features], axis=0))
 
-        self.logger.debug("Adj: " + str(self.adj.shape))
-        self.logger.debug("Node features: " + str(node_features.shape))
-
         gcn_out = self.gcn1([node_features, self.adj])
-        gcn_out = self.gcn2([gcn_out, self.adj])
-        self.logger.debug("GCN Output: {}".format(gcn_out.shape))
+        gcn_out = self.gcn2([gcn_out, self.adj])  # [B,N,16]
+
         noconv_gcn_out = tf.identity(gcn_out)
-        noconv_gcn_out = Flatten(data_format="channels_last")(noconv_gcn_out)
+        noconv_gcn_out = tf.keras.layers.Flatten(data_format="channels_last")(
+            noconv_gcn_out
+        )
 
         if not training:  # save gcn maps for interpretation
             self.gcnmaps.assign(tf.concat([self.gcnmaps, gcn_out], axis=0))
@@ -177,27 +174,27 @@ class GCLSTM(tf.keras.Model):
         gcn_out = self.conv1(gcn_out)
         gcn_out = self.pooling(gcn_out)
         first_conv_out = tf.identity(gcn_out)
+        gcn_out = tf.nn.relu(gcn_out)
         gcn_out = self.conv2(gcn_out)
 
-        gcn_out = Flatten(data_format="channels_first")(gcn_out)
-        first_conv_out = Flatten(data_format="channels_first")(first_conv_out)
-
+        # add & norm
         if not self.ablation == "noskip":
-            gcn_out = tf.concat([gcn_out, first_conv_out], axis=-1)
+            gcn_out = gcn_out + first_conv_out
+            gcn_out = self.bn(gcn_out)
+
+        # flatten
+        gcn_out = tf.reshape(gcn_out, (-1))
 
         lstm_out = self.lstm1(inputs[:, :, self.node, :])
-        lstm_out = self.lstm2(lstm_out)
-        self.logger.debug("LSTM Out: " + str(lstm_out.shape))
+        lstm_out = self.lstm2(lstm_out)  # [B,128]
 
         if not self.ablation == "noconv":
-            out = tf.concat((gcn_out, lstm_out), axis=-1)
+            out = tf.concat((gcn_out, lstm_out), axis=-1)  # [B,192]
         else:
             out = tf.concat((noconv_gcn_out, lstm_out), axis=-1)
-        self.logger.debug("Concatenation: " + str(out.shape))
 
         out = self.dropout(out)
-        out = self.out(out)
-        self.logger.debug("Out: " + str(out.shape))
+        out = self.out(out)  # [B,P]
 
         return out
 
@@ -209,10 +206,8 @@ class GCLSTM(tf.keras.Model):
             y = y[:, :, self.node]  # Get only the active node
             loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
 
-        # Compute gradients and update weights
-        trainable_vars = self.trainable_variables
-        gradients = tape.gradient(loss, trainable_vars)
-        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
 
         # Update metrics (includes the metric that tracks the loss)
         self.compiled_metrics.update_state(y, y_pred)
@@ -222,7 +217,6 @@ class GCLSTM(tf.keras.Model):
             with self.w.as_default():
                 tf.summary.histogram("y", y)
 
-        # Return a dict mapping metric names to current value
         return {m.name: m.result() for m in self.metrics}
 
     def test_step(self, data):
@@ -234,7 +228,6 @@ class GCLSTM(tf.keras.Model):
         self.compiled_loss(y, y_pred, regularization_losses=self.losses)
         self.compiled_metrics.update_state(y, y_pred)
 
-        # Return a dict mapping metric names to current value.
         return {m.name: m.result() for m in self.metrics}
 
 
@@ -242,8 +235,7 @@ class GCLSTM_oneclass(tf.keras.Model):
     def __init__(self, h, p, n, adj=None):
         super(GCLSTM_oneclass, self).__init__()
 
-        self.logger = logging.getLogger(__name__)
-        self.logger.info(f"Initializing {__name__}.")
+        print(f"Initializing {__name__}.")
 
         self.h = h
         self.p = p
@@ -253,15 +245,18 @@ class GCLSTM_oneclass(tf.keras.Model):
         self.node = 0
 
         self.mappers = [
-            Dense(32, activation="relu", input_shape=(h, self.real_f[node]))
+            tf.keras.layers.Dense(
+                32, activation="relu", input_shape=(h, self.real_f[node])
+            )
             for node in range(n)
         ]
 
-        self.encoder = Dense(8, activation="relu")
-        self.decoder = Dense(32, activation="relu")
+        self.encoder = tf.keras.layers.Dense(8, activation="relu")
+        self.decoder = tf.keras.layers.Dense(32, activation="relu")
 
         self.demappers = [
-            Dense(self.real_f[node], activation="relu") for node in range(n)
+            tf.keras.layers.Dense(self.real_f[node], activation="relu")
+            for node in range(n)
         ]
 
     def set_adj(self, adj):
@@ -269,16 +264,16 @@ class GCLSTM_oneclass(tf.keras.Model):
 
     def set_node(self, node):
         self.node = node
-        self.logger.info(f"Node {node} set for training in {__name__}.")
+        print(f"Node {node} set for training in {__name__}.")
 
     def call(self, inputs):
-        self.logger.warning(
+        print(
             "\n\n\nInputs: " + str(inputs.shape)
         )  # [B,H,N,F], F corrisponde al max_n(F), ovvero al numero di feature del nodo che ne ha di più. I NaN sono usati come padding.
         B, H, N, F = inputs.shape
         """
         if self.real_F is None:
-            self.logger.info('Computing the real F...')
+            print('Computing the real F...')
             real_F = []
 
             for n in range(N):  # Controllo feature "vere", in base al padding dei NaN
@@ -286,7 +281,7 @@ class GCLSTM_oneclass(tf.keras.Model):
                 f = first_nan_idx if first_nan_idx is not None else F
                 real_F.append(f)
             self.real_F = tf.Variable(real_F, trainable=False)
-            self.logger.info(f'real_F[0]: {real_F[0]}')
+            print(f'real_F[0]: {real_F[0]}')
         """
         print(f"Inputs contains nan?: {tf.math.reduce_any(tf.math.is_nan(inputs))}")
 
@@ -294,20 +289,20 @@ class GCLSTM_oneclass(tf.keras.Model):
             self.mappers[n](inputs[:, :, n, : self.real_f[n]]) for n in range(N)
         ]
         common_space_mapping = tf.stack(common_space_mapping, axis=2)  # [B,H,N,32]
-        self.logger.info(f"common_space_mapping.shape: {common_space_mapping.shape}")
+        print(f"common_space_mapping.shape: {common_space_mapping.shape}")
 
         encoded = self.encoder(common_space_mapping)  # [B,H,N,8]
-        self.logger.info(f"encoded.shape: {encoded.shape}")
+        print(f"encoded.shape: {encoded.shape}")
 
         decoded = self.decoder(encoded)  # [B,H,N,32]
-        self.logger.info(f"decoded.shape: {decoded.shape}")
+        print(f"decoded.shape: {decoded.shape}")
 
         demapped = [self.demappers[n](decoded[:, :, n]) for n in range(N)]
         demapped = [
             tf.pad(d, paddings=[[0, 0], [0, 0], [0, F - d.shape[-1]]]) for d in demapped
         ]
         demapped = tf.stack(demapped, axis=2)
-        self.logger.info(f"demapped.shape: {demapped.shape}")
+        print(f"demapped.shape: {demapped.shape}")
 
         print(f"Output contains nan?: {tf.math.reduce_any(tf.math.is_nan(demapped))}")
 
